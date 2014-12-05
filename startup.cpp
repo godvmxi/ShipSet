852a451bedfdfa464ee516c51faec766b45ac42f
godvmxi <godvmxi@126.com>
2014-12-05 11:02:56 +0800

    add debug for start up

--- startup.cpp
+++ startup.cpp
@@ -2,15 +2,23 @@
 #include <QProcess>
 #include<QDebug>
 
+
 int main(int argc, char *argv[])
 {
+#if 0
+    qDebug()<<"hello qDebug";
+    qWarning()<<"hello qWarning";
+    qCritical()<<"hello qCritical";
+//    qFatal()   <<"hello qFatal"; //can use like this
+    qFatal("hello qFatal");
+#endif
+
     QCoreApplication a(argc, argv);
     QProcess *process = new QProcess();
     process->start("ShipSet.exe");
     if( process->waitForStarted(3000)  ){
         qDebug()<<"start ok";
         process->waitForFinished();
-
     }
     delete process;
 }
